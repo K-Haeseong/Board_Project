994cf0f285be40ecffd4da2ae51dbf44adb34ad4
Aiden <kim_aiden@naver.com>
2024-01-17 17:24:46 +0900

    feat : 게시글 리스트 조회 기능 추가 - PostController > openPostList() 추가 - list.html 추가

--- Board/src/main/java/com/study/domain/post/PostController.java
+++ Board/src/main/java/com/study/domain/post/PostController.java
@@ -1,10 +1,14 @@
 package com.study.domain.post;
 
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.PackagePrivate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,6 +16,7 @@ public class PostController {
 
     private final PostService postService;
 
+    // 게시글 작성
     @GetMapping("/post/write")
     public String openPostWrite(@RequestParam(value = "id", required = false)Long id, Model model) {
 
@@ -21,4 +26,21 @@ public class PostController {
         }
         return "post/write";
     }
+
+    // 신규 게시글 생성
+    @PostMapping("/post/save")
+    public String savePost(PostRequest params) {
+        postService.savePost(params);
+        return "redirect:/post/list";
+    }
+
+
+    @GetMapping("/post/list")
+    public String openPostList(Model model) {
+        List<PostResponse> posts = postService.findAllPost();
+        model.addAttribute("posts", posts);
+        return "post/list";
+
+    }
+
 }
