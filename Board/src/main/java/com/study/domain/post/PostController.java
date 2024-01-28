c707678e887feeccd98521c5d176570e4a6a32c7
Aiden <kim_aiden@naver.com>
2024-01-28 16:52:42 +0900

    feat : 페이징 & 검색처리(페이지네이션 적용중) - dto > paging 생성: Pagination.class / PagingResponse.class 추가 - post > PostController / PostService 추가 - dto > SearchDto 변경 - mappers > PostMapper.xml > findAll 쿼리 변경

--- Board/src/main/java/com/study/domain/post/PostController.java
+++ Board/src/main/java/com/study/domain/post/PostController.java
@@ -2,6 +2,7 @@ package com.study.domain.post;
 
 import com.study.common.dto.MessageDto;
 import com.study.common.dto.SearchDto;
+import com.study.common.paging.PagingResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -42,8 +43,8 @@ public class PostController {
     // 게시글 리스트
     @GetMapping("/post/list")
     public String openPostList(SearchDto params, Model model) {
-        List<PostResponse> posts = postService.findAllPost(params);
-        model.addAttribute("posts", posts);
+        PagingResponse<PostResponse> response  = postService.findAllPost(params);
+        model.addAttribute("response ", response);
         return "post/list";
     }
 
--- Board/src/main/java/com/study/domain/post/PostService.java
+++ Board/src/main/java/com/study/domain/post/PostService.java
@@ -1,11 +1,14 @@
 package com.study.domain.post;
 
 import com.study.common.dto.SearchDto;
+import com.study.common.paging.Pagination;
+import com.study.common.paging.PagingResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -15,10 +18,10 @@ public class PostService {
     private final PostMapper postMapper;
 
 
-    /*게시글 저장
-    * param - PostRequest
-    * return - id
-    * */
+    /* 게시글 저장
+     * param - PostRequest
+     * return - id
+     * */
     @Transactional
     public Long savePost(PostRequest params) {
         postMapper.save(params);
@@ -26,7 +29,7 @@ public class PostService {
     }
 
 
-    /*게시글 상세 조회
+    /* 게시글 상세 조회
      * param - id
      * return - PostResponse
      * */
@@ -35,7 +38,7 @@ public class PostService {
     }
 
 
-    /*게시글 수정
+    /* 게시글 수정
      * param - PostRequest
      * return - id
      * */
@@ -46,7 +49,7 @@ public class PostService {
     }
 
 
-    /*게시글 삭제
+    /* 게시글 삭제
      * param - id
      * return - id
      * */
@@ -56,12 +59,25 @@ public class PostService {
     }
 
 
-    /*게시글 목록 조회
-     * param - PostRequest
-     * return - id
+    /* 게시글 목록 조회
+     * param - SearchDto
+     * return - list & pagination information
      * */
 
-    public List<PostResponse> findAllPost(SearchDto params){
-        return postMapper.findAll(params);
+    public PagingResponse<PostResponse> findAllPost(SearchDto params){
+
+        // 조건에 해당하는 데이터가 없으면, 응답 데이터에 빈 리스트 & null 반환
+        int count = postMapper.count(params);
+        if(count < 1) {
+            return new PagingResponse<>(Collections.emptyList(), null);
+        }
+
+        // Pagination 객체에 페이지 정보 계산 후 SearchDto에 페이지 정보 저장
+        Pagination pagination = new Pagination(count, params);
+        params.setPagination(pagination);
+
+        // limitStart, recordSize를 기준으로 리스트 조회
+        List<PostResponse> list = postMapper.findAll(params);
+        return new PagingResponse<>(list, pagination);
     }
 }
