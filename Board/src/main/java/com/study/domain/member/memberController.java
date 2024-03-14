e25e9573dc83a01640347e124416e89cce2136fd
Aiden <kim_aiden@naver.com>
2024-03-14 23:57:21 +0900

    feat : 로그인/로그아웃 & 로그인 세션 체크 기능 구현 - LoginCheckInterceptor 등록 - login.html > 로그인 함수 추가

--- Board/src/main/java/com/study/domain/member/memberController.java
+++ Board/src/main/java/com/study/domain/member/memberController.java
@@ -2,7 +2,6 @@ package com.study.domain.member;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -15,41 +14,10 @@ public class memberController {
     // final로 선언된 필드는 반드시 객체 생성 시점 또는 생성자에서 초기화되어야 한다.
     // 초기화를 강제하고, 객체의 유효한 상태를 보장하는데 도움이 된다.
 
-    /* 로그인 페이지 */
+    // 로그인 페이지
     @GetMapping("/login")
     public String openLogin() {
         return "member/login";
-    }
-
-    @PostMapping("/members")
-    @ResponseBody
-    /* 회원 정보 저장(회원가입)*/
-    public Long memberSave(@RequestBody MemberRequest params) {
-        return memberService.saveMember(params);
-    }
-
-    @GetMapping("/members/{loginId}")
-    @ResponseBody
-    /* 회원 상세 정보 조회 */
-    public MemberResponse findMemberByLoginId(@PathVariable String loginId) {
-        return memberService.findMemberByLoginId(loginId);
-    // loginId로 구분 하는 이유
-    //
-    // 개인화 및 식별성: 로그인 아이디나 사용자명을 사용하여 회원을 식별하는 것은 해당 회원의 개인화된 정보를 제공하고, 다른 사용자와의 혼동을 방지하는 데 도움된다.
-    //
-    // 유지보수성: 만약 데이터베이스 스키마가 변경되어 PK가 바뀌거나 다른 식별자로 변경되더라도,
-    //           로그인 아이디와 같은 비즈니스적으로 중요한 정보를 기준으로 조회하면 해당 변경에 영향을 덜 받을 수 있다.
-    //
-    // 보안: PK는 주로 데이터베이스 내부에서 사용되는 값이며, 외부에 노출되는 경우가 제한적이다.
-    //      반면 로그인 아이디나 사용자명은 사용자에게 제공되는 정보이므로 보다 안전하게 활용될 수 있다.
-    //      자세한건 <회원아이디를 URL의 식별자로 쓰는 이유 / PK 노출 위험성>정리본 참고
-    }
-
-    /* 회원 수 세기(중복체크) */
-    @GetMapping("/member-count")
-    @ResponseBody
-    public int countMemberByLoginId(@RequestParam("loginId") String loginId) {
-        return memberService.countMemberByLoginId(loginId);
     }
 
 
@@ -81,5 +49,35 @@ public class memberController {
     }
 
 
+    @PostMapping("/members")
+    @ResponseBody
+    /* 회원 정보 저장(회원가입)*/
+    public Long memberSave(@RequestBody MemberRequest params) {
+        return memberService.saveMember(params);
+    }
+
+    @GetMapping("/members/{loginId}")
+    @ResponseBody
+    /* 회원 상세 정보 조회 */
+    public MemberResponse findMemberByLoginId(@PathVariable String loginId) {
+        return memberService.findMemberByLoginId(loginId);
+    // loginId로 구분 하는 이유
+    //
+    // 개인화 및 식별성: 로그인 아이디나 사용자명을 사용하여 회원을 식별하는 것은 해당 회원의 개인화된 정보를 제공하고, 다른 사용자와의 혼동을 방지하는 데 도움된다.
+    //
+    // 유지보수성: 만약 데이터베이스 스키마가 변경되어 PK가 바뀌거나 다른 식별자로 변경되더라도,
+    //           로그인 아이디와 같은 비즈니스적으로 중요한 정보를 기준으로 조회하면 해당 변경에 영향을 덜 받을 수 있다.
+    //
+    // 보안: PK는 주로 데이터베이스 내부에서 사용되는 값이며, 외부에 노출되는 경우가 제한적이다.
+    //      반면 로그인 아이디나 사용자명은 사용자에게 제공되는 정보이므로 보다 안전하게 활용될 수 있다.
+    //      자세한건 <회원아이디를 URL의 식별자로 쓰는 이유 / PK 노출 위험성>정리본 참고
+    }
+
+    /* 회원 수 세기(중복체크) */
+    @GetMapping("/member-count")
+    @ResponseBody
+    public int countMemberByLoginId(@RequestParam("loginId") String loginId) {
+        return memberService.countMemberByLoginId(loginId);
+    }
 
 }
--- Board/src/main/java/com/study/domain/member/memberService.java
+++ Board/src/main/java/com/study/domain/member/memberService.java
@@ -32,19 +32,21 @@ public class memberService {
 
 
     // 로그인
-    public MemberResponse login(final String loginId, final String password) {
+    public MemberResponse login(String loginId, String password) {
 
-        // 1. 회원 정보 및 비밀번호 조회
+        // 회원 정보 및 비밀번호 조회
         MemberResponse member = findMemberByLoginId(loginId);
         String encodedPassword = (member == null) ? "" : member.getPassword();
 
-        // 2. 회원 정보 및 비밀번호 체크
-        if (member == null || passwordEncoder.matches(password, encodedPassword) == false) {
+        // 회원 정보 및 비밀번호 체크
+        if ( member == null || passwordEncoder.matches(password, encodedPassword)) {
             return null;
         }
 
-        // 3. 회원 응답 객체에서 비밀번호를 제거한 후 회원 정보 리턴
         member.clearPassword();
         return member;
+
     }
+
+
 }
